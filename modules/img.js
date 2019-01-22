bf213faf0b389872345b61ac2b1b3e4ca4f56762
dehero <dehero@outlook.com>
2019-01-22 15:55:26 +0300

    Fix when image size lower than limits

--- modules/img.js
+++ modules/img.js
@@ -1,19 +1,19 @@
 window.imgSizeGetResized = function(width, height, widthMax, heightMax) {
-    if (varIsNotEmpty(widthMax) && varIsNotEmpty(heightMax)) {
-        height = Math.round(widthMax / width * height);
+	if (varIsNotEmpty(widthMax) && varIsNotEmpty(heightMax) && (width > widthMax || height > heightMax)) {		
+		height = Math.round(widthMax / width * height);
 
-        if (height > heightMax) {
-            width = Math.round(heightMax / height * widthMax);
-            height = heightMax;
-        } else {
-            width = widthMax;
-        }
-    } else if (varIsNotEmpty(widthMax)) {
-        height = Math.round(widthMax / width * height);
-        width = widthMax;
-    } else if (varIsNotEmpty(heightMax)) {
-        width = Math.round(heightMax / height * width);
-        height = heightMax;
-    }
+		if (height > heightMax) {
+			width = Math.round(heightMax / height * widthMax);
+			height = heightMax;
+		} else {
+			width = widthMax;
+		}
+	} else if (varIsNotEmpty(widthMax) && width > widthMax) {
+		height = Math.round(widthMax / width * height);
+		width = widthMax;
+	} else if (varIsNotEmpty(heightMax) && height > heightMax) {
+		width = Math.round(heightMax / height * width);
+		height = heightMax;
+	}
     return [width, height];
 }
