cccedfbd5117de41bd8890da406b95f49732df33
dehero <dehero@outlook.com>
2019-02-27 16:29:49 +0300

    Update datetime.js

--- modules/datetime.js
+++ modules/datetime.js
@@ -1,26 +1,125 @@
-window.datetimeGetModified = function(datetime, year, month, day, hour, minute, second) {
-    var date = new Date(datetime);
+window.datetime = function(value) {
+    if (varIsStr(value)) {
+        value = datetimeObj(value);
+    }
+    if (varIsObj(value)) {
+        if (typeof value.toISOString === 'function') {
+            var str = value.toISOString();
 
-    if (typeof year !== 'undefined') {
-        date.setUTCFullYear(date.getUTCFullYear() + year);
+            return str.substring(0, 10) + ' ' + str.substring(11, 19);            
+        } else {
+            fastredRequire('int');
+            value = datetimeObj(value);
+
+            return intToStr(value.year, 4) + '-' + intToStr(value.month, 2) + '-' + intToStr(value.day, 2) + ' ' +
+                   intToStr(value.hour, 2) + ':' + intToStr(value.minute, 2) + ':' + intToStr(value.second, 2);
+        }
+    } else {
+        var date = varIsNumber(value) ? new Date(value) : new Date();
+        var str = date.toISOString();
+
+        return str.substring(0, 10) + ' ' + str.substring(11, 19);
     }
-    if (typeof month !== 'undefined') {
-        date.setUTCMonth(date.getUTCMonth() + month);
+};
+window.datetimeObj = function(value, month, day, hour, minute, second) {
+    fastredRequire('int', 'var');
+
+    var year;
+        
+    if (varIsObj(value)) {
+        year = value.year;
+        month = value.month;
+        day = value.day;
+        hour = value.hour;
+        minute = value.minute;
+        second = value.second;
+    } else if (varIsStr(value)) {
+        return datetimeObjFromStr(value);
+    } else {
+        year = value;
     }
-    if (typeof day !== 'undefined') {
-        date.setUTCDate(date.getUTCDate() + day);
+    
+    var result = {};
+
+    result.year   = intFromStr(year);
+    result.month  = intFromStr(month);
+    result.day    = intFromStr(day);
+    result.hour   = intFromStr(hour);
+    result.minute = intFromStr(minute);
+    result.second = intFromStr(second);
+
+    if (result.year < 1)    {result.year = 1}
+    if (result.day < 1)     {result.day = 1}
+    if (result.month < 1)   {result.month = 1}
+
+    return result;
+
+};
+window.datetimeObjFromStr = function(str) {    
+    var matches;
+    var year;
+    var month;
+    var day;
+    var hour;
+    var minute;
+    var second;
+
+    if (varIsEmpty(str)) {
+        return datetimeObj();
     }
-    if (typeof hour !== 'undefined') {
-        date.setUTCHours(date.getUTCHours() + hour);
+    
+    if (matches = str.match(/(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/)) {
+        // 2019-02-19 11:55:05    
+        year   = matches[1];
+        month  = matches[2];
+        day    = matches[3];
+        hour   = matches[4];
+        minute = matches[5];
+        second = matches[6];
+    } else if(matches = str.match(/(\d{4})-(\d{2})-(\d{2})/)) {
+        // 2019-02-19
+        year    = matches[1];
+        month   = matches[2];
+        day     = matches[3];
+    } else if(matches = str.match(/(\d{2}):(\d{2}):(\d{2})/)) {
+        // 13:16:19
+        hour    = matches[1];
+        minute  = matches[2];
+        second  = matches[3];
     }
-    if (typeof minute !== 'undefined') {
-        date.setUTCMinutes(date.getUTCMinutes() + minute);
+
+    return datetimeObj(parseInt(year), month, day, hour, minute, second);
+}
+window.datetimeGetModified = function(datetime, year, month, day, hour, minute, second) {   
+    var obj = datetimeObj(datetime);
+
+    var date = new Date(Date.UTC(obj.year, obj.month - 1, obj.day, obj.hour, obj.minute, obj.second));
+
+    if (varIsNotEmpty(year)) {        
+        date.setUTCFullYear(obj.year + year);
     }
-    if (typeof second !== 'undefined') {
-        date.setUTCSeconds(date.getUTCSeconds() + second);
+    if (varIsNotEmpty(month)) {
+        date.setUTCMonth(obj.month - 1 + month);
+    }
+    if (varIsNotEmpty(day)) {
+        date.setUTCDate(obj.day + day);
+    }
+    if (varIsNotEmpty(hour)) {
+        date.setUTCHours(obj.hour + hour);
+    }
+    if (varIsNotEmpty(minute)) {
+        date.setUTCMinutes(obj.minute + minute);
+    }
+    if (varIsNotEmpty(second)) {
+        date.setUTCSeconds(obj.second + second);
     }
 
     var str = date.toISOString();
 
     return str.substring(0, 10) + ' ' + str.substring(11, 19);
+};
+window.datetimeGetWeekday = function(datetime) {
+    var date = new Date(datetime);
+
+    return date.getDay();
 };
