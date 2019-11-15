266809d3b65250f4930d692c16a5e9860dfaeea9
Anton Kuryanov <kuryanov_av@nlmk.com>
2019-11-15 09:43:42 +0300

    Add error messages

--- modules/app.php
+++ modules/app.php
@@ -13,26 +13,26 @@ if (!defined('APP_CACHE_PATH')) {
 if (!function_exists('app')) {
     function app() {
         fastredRequire('cache');
-    
+
         return cache('app', function() {
             $app = new stdClass();
-    
+
             $app->debug = true;
-    
+
             return $app;
         });
     }
 }
 
 if (!function_exists('appErrorHandle')) {
-    function appErrorHandle() {        
+    function appErrorHandle() {
         try {
             appError(app()->statusCode);
         } catch (Exception $e) {
             $app = app();
-    
+
             if (empty($app->statusCode) || $app->statusCode < 400) $app->statusCode = 500;
-            
+
             appStatusCodeHeader($app->statusCode);
             echo "<html><head></head><body><h1>HTTP Error $app->statusCode</h1><pre>$app->debugOutput</pre><address></body></html>";
             die();
@@ -49,10 +49,10 @@ if (!function_exists('appCtrlToRoute')) {
 }
 
 if (!function_exists('appError')) {
-    function appError($errorCode = 404) {
+    function appError($errorCode = 404, $message = '') {
         $app = app();
         $app->statusCode = $errorCode;
-        throw new Exception('', $errorCode);
+        throw new Exception($message, $errorCode);
     }
 }
 
@@ -74,26 +74,26 @@ if (!function_exists('appRun')) {
     function appRun() {
         try {
             ob_start();
-    
-            $app = app();
-    
-            $app->startTime = microtime(true);
-    
-            // Get initial route and exclude starting and ending slashes
-            $route = trim(scriptGetRoute(), '/');    
 
-            fastredRequire('page');                      
+            $app = app();
+
+            $app->startTime = microtime(true);
+
+            // Get initial route and exclude starting and ending slashes
+            $route = trim(scriptGetRoute(), '/');
+
+            fastredRequire('page');
 
             $params = pageParamsFromRoute($route, $_SERVER['HTTP_HOST']);
-    
+
             // Deny accessing route, if it doesn't turn to controller
             if (is_null($params)) appError(404);
-            
+
             pageParamsLoadGetArr($params, $_GET);
 
             $app->page = page($params);
 
-            $statusCode = pageHandle($app->page);      
+            $statusCode = pageHandle($app->page);
 
             $app->debugOutput = ob_get_clean();
 
@@ -101,11 +101,11 @@ if (!function_exists('appRun')) {
             if (appStatusCodeIsError($statusCode)) {
                 appError($statusCode);
             }
-    
+
             if (!$app->debug) ini_set('display_errors', 0);
-    
+
             pageRender($app->page);
-    
+
         } catch (Exception $e) {
             $app = app();
             $app->debugOutput = ob_get_clean();
